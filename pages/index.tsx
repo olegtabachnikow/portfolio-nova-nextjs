00d80fcd3ffa3e6ec6e98b8547b49589ac8eb7e3
OlegTabachnikow <eskelll4ik@gmail.com>
2024-04-12 21:56:39 +0300

    updated meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,40 @@
 import { GetStaticProps } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import AboutContent from '@/components/AboutContent/AboutContent';
-import Head from 'next/head';
 import { useTranslation } from 'react-i18next';
 
 export default function Page() {
-  const { i18n } = useTranslation();
-  return (
-    <>
-      <Head>
-        <meta
-          property='og:title'
-          content="Oleg's frontent web developer portfolio"
-        />
-        <meta
-          property='og:description'
-          content="Oleg's frontent web developer portfolio"
-        />
-        <meta property='og:image' content='/image/preview.png' />
-        <meta
-          property='og:url'
-          content={`https://oleg-web-dev.vercel.app/${i18n.language}`}
-        />
-        <meta property='og:type' content='website' />
-      </Head>
-      <AboutContent />
-    </>
-  );
+  return <AboutContent />;
 }
+
+export const metadata = {
+  generator: 'Next.js',
+  applicationName: "Oleg's frontent web developer portfolio",
+  referrer: 'origin-when-cross-origin',
+  keywords: [
+    'Next.js',
+    'React',
+    'JavaScript',
+    'TypeScript',
+    'HTML',
+    'CSS',
+    'Portfolio',
+    'Frontend',
+    'Web Developer',
+    'Oleg Tabachnikow',
+  ],
+  authors: [{ name: 'Oleg Tabachnikow' }],
+  creator: 'Oleg Tabachnikow',
+  publisher: 'Oleg Tabachnikow',
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  openGraph: {
+    images: '/images/preview.png',
+  },
+};
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => {
   const translationsProps = await serverSideTranslations(locale ?? 'en', [
